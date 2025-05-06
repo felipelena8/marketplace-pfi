a78d884cd2491e54e9109c313804d784a984166a
Felipe Costa <felipelena8@gmail.com>
2025-05-06 20:45:59 -0300

    a

---
+++ src/main/java/com/uade/marketplace/notifications/config/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package com.uade.marketplace.notifications.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@EnableScheduling
+public class CorsConfiguration implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+  }
+}
