2acc0a793c6aeb604207c5dbd986a7032aa56d85
Felipe Costa <felipelena8@gmail.com>
2025-05-05 22:39:53 -0300

    a

--- src/main/java/com/uade/marketplace/base/config/AWSConfig.java
+++ src/main/java/com/uade/marketplace/base/config/AWSConfig.java
@@ -13,22 +13,21 @@ import org.springframework.context.annotation.Configuration;
 public class AWSConfig {
 
   @Value("${aws.access-key-id}")
-  private String awsId;
+  private String accessKeyId;
 
   @Value("${aws.secret-access-key}")
-  private String awsKey;
+  private String secretAccessKey;
+
+  @Value("${aws.session-token}")
+  private String awsSessionToken;
 
   @Value("${aws.region}")
   private String region;
 
   @Bean
   public AmazonSQS sqsClient() {
-    BasicSessionCredentials basicAWSCredentials =
-        new BasicSessionCredentials("ASIA3D6HGFMM3ACPYCRB", "KU7qeCicp1vJQkx2GjgD90CpggKe+g6ddvqJ5ofV",
-            "IQoJb3JpZ2luX2VjEJL//////////wEaCXVzLXdlc3QtMiJIMEYCIQCL+kAsumstnpki+1vESyosSzxuas5Ld3nk0GcZZPApngIhAMv1/IIOXu6yu74V+GpIHlBzlotiXRfZn8014SCMxOq6KqgCCDsQARoMNzY0Mzg1MDQ1MjczIgzefd+aa+3aaJgsBN0qhQLxPRV5uQemF+vB4oa555V+a4CxHtXEOsbd3XSekPdJBcOst/ywdWDjwxgLSZ7KpzAxL3hDX0Kmy6vpzDeOp3CsAS4K8OahwfyC2fCr8txRJkNf5AHDFEd+TAzHwUixrmAV6tmMyvlkNEPMBRWonKVkfI94o7M0+TAs46qMOrPXrJFkHUkqP53zvOoK2B69BGYqqlTTKz/pumCh+oy9eYus06yMZkpOFb/uBm2IyBeNpEvkzBZdldHTNv2DbX+e6wqIh+oWbBu0D71T6d+eUG+9I5h6MNuntPije8/X3YgsHNQWzaJtys5V2kJfeMIU5pTyk62c34AvnzcIgjOVVBD6yOrVpUow+snlwAY6nAEgvUyNK7YyBBkQDZRzXRaU2Uo8bHY8gFTPKk0jJhYAb588MEunao8O3gjCPHWRU8rCyxnaFoucUP1UK13+WK+XXmfoRTFbanEhE76u5jkqwiN0egNrvu7cy+K/QZoezM/UQoSnF4NqtjIelq6b4wn4Ng2ICeO1wwvjkI3mgKFNzH7Et++GYI+Uv60y23Ghi2K8EFm1hejswsBx1xY=");
-    return AmazonSQSClientBuilder.standard()
-        .withRegion(Regions.fromName(region))
-        .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
-        .build();
+    BasicSessionCredentials basicAWSCredentials = new BasicSessionCredentials(accessKeyId, secretAccessKey, awsSessionToken);
+    return AmazonSQSClientBuilder.standard().withRegion(Regions.fromName(region))
+        .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials)).build();
   }
 }
