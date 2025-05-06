3107320d9b917f9756ed4a0cad7f5197b9fa9d63
felipelena8 <77079122+felipelena8@users.noreply.github.com>
2025-05-05 22:29:59 -0300

    Se modifican credenciales (#3)

--- src/main/java/com/uade/marketplace/base/config/AWSConfig.java
+++ src/main/java/com/uade/marketplace/base/config/AWSConfig.java
@@ -24,8 +24,8 @@ public class AWSConfig {
   @Bean
   public AmazonSQS sqsClient() {
     BasicSessionCredentials basicAWSCredentials =
-        new BasicSessionCredentials("ASIA3D6HGFMMTTIJBVWU", "16DKzwPUiLx2MPEYFoHx+6bW2Md5rNwYv2+B1KgK",
-            "IQoJb3JpZ2luX2VjEHQaCXVzLXdlc3QtMiJHMEUCIQCH+lepjQDU8BY4zBrDUOeNk9aKSxF77t/jLIZofC6nEAIgW6j92gazUIOVD7+te1ds2CjjHL8QsccROCdOf9y7TsIqqAIIHRABGgw3NjQzODUwNDUyNzMiDFWxtisIpvtER2sbLCqFAq8sFo0OZV+0UxLIyau2Fhs3XfaWHHRVW0/cAO5Lz10l8Kr7r3UfFNYQ2xZJ2MULEu38PbFJaNvqbywDNmsMn9Jfma3fg7+ZgIieK2XO78cqmSn/5bfQt8OW4da241QMYuzyf4Ujigp+v5YX3L1Wg6p3nQI3qx3M1wdaejZgnDpXgD2rekTsGi1cI0sGqYYG5VhlW6+05Ujy9Xp7KNKTf3+B7OPzk+OGxxxTqCEW2Miakidj/7mjC8rSGdgv9BZBOKXnloVaPZKbwwGQoHmJ3LaBiT98sBSWIYJtPFUTk60bK2hx4DQN94STqoburzzu5pGepW97J/9tBaV4fVeZMl8IPdTROzCrlN/ABjqdAXJnvbcinwv6tRX96WFvPzPwV5F+jNcjzrlv019uzmTmIaDj9AjHP4wqR2Sw6rSrLBrMXOcNEB2Ar3N2mIq12wax5SESkURSH/UrZ1Yk2g2wecwO2AfTn+n7kRaujRzFEUrfAc2QqTyYGOoWAKqQfWMSg25LCSX7TFOYXhaunNKZyClNafVR42qlcE7KnokHv9RsSB7sr+HOS6w5c/Y=");
+        new BasicSessionCredentials("ASIA3D6HGFMM3ACPYCRB", "KU7qeCicp1vJQkx2GjgD90CpggKe+g6ddvqJ5ofV",
+            "IQoJb3JpZ2luX2VjEJL//////////wEaCXVzLXdlc3QtMiJIMEYCIQCL+kAsumstnpki+1vESyosSzxuas5Ld3nk0GcZZPApngIhAMv1/IIOXu6yu74V+GpIHlBzlotiXRfZn8014SCMxOq6KqgCCDsQARoMNzY0Mzg1MDQ1MjczIgzefd+aa+3aaJgsBN0qhQLxPRV5uQemF+vB4oa555V+a4CxHtXEOsbd3XSekPdJBcOst/ywdWDjwxgLSZ7KpzAxL3hDX0Kmy6vpzDeOp3CsAS4K8OahwfyC2fCr8txRJkNf5AHDFEd+TAzHwUixrmAV6tmMyvlkNEPMBRWonKVkfI94o7M0+TAs46qMOrPXrJFkHUkqP53zvOoK2B69BGYqqlTTKz/pumCh+oy9eYus06yMZkpOFb/uBm2IyBeNpEvkzBZdldHTNv2DbX+e6wqIh+oWbBu0D71T6d+eUG+9I5h6MNuntPije8/X3YgsHNQWzaJtys5V2kJfeMIU5pTyk62c34AvnzcIgjOVVBD6yOrVpUow+snlwAY6nAEgvUyNK7YyBBkQDZRzXRaU2Uo8bHY8gFTPKk0jJhYAb588MEunao8O3gjCPHWRU8rCyxnaFoucUP1UK13+WK+XXmfoRTFbanEhE76u5jkqwiN0egNrvu7cy+K/QZoezM/UQoSnF4NqtjIelq6b4wn4Ng2ICeO1wwvjkI3mgKFNzH7Et++GYI+Uv60y23Ghi2K8EFm1hejswsBx1xY=");
     return AmazonSQSClientBuilder.standard()
         .withRegion(Regions.fromName(region))
         .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
